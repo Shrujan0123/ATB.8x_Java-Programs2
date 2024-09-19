0a53be9d90d6afb90779dabab0b27734ee682c3e
Shrujan0123 <shrujankumar01@gmail.com>
2024-09-19 18:00:31 +0530

    Add the files

---
+++ src/sept/ex_18092024/Task4_2.java
@@ -0,0 +1,11 @@
+package sept.ex_18092024;
+
+public class Task4_2 {
+    public static void main(String[] args){
+        //GST =18.45;
+        int course_fees=100;
+        float GST=18.45f;
+        int total_price=course_fees+(int)GST;
+        System.out.println(total_price);
+    }
+}
