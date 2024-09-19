7a4843ef5987ec64f33c97a70ad031a266154ff2
Shrujan0123 <shrujankumar01@gmail.com>
2024-09-19 17:58:44 +0530

    Add the files

---
+++ src/sept/ex_18092024/Task4.java
@@ -0,0 +1,10 @@
+package sept.ex_18092024;
+
+public class Task4 {
+    public static void main(String[] args){
+        int total_marks=60;
+        float my_marks=47.5f;
+        float percentage=((float)total_marks/my_marks)*100.0f;
+        System.out.println(("My percentage is " +percentage));
+    }
+}
